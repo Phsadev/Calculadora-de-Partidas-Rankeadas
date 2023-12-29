c161cc9f7058dbeb73b75bcdd07a55be81beb901
Phsadev <phsadev@hotmail.com>
2023-12-29 12:47:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+index.js
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (saldoVitorias < 10) {
+        nivel = 'Ferro';
+    } else if (saldoVitorias <= 20) {
+        nivel = 'Bronze';
+    } else if (saldoVitorias <= 50) {
+        nivel = 'Prata';
+    } else if (saldoVitorias <= 80) {
+        nivel = 'Ouro';
+    } else if (saldoVitorias <= 90) {
+        nivel = 'Diamante';
+    } else if (saldoVitorias <= 100) {
+        nivel = 'Lendário';
+    } else {
+        nivel = 'Imortal';
+    }
+
+    return "O Herói tem de saldo de " + saldoVitorias + " e está no nível de " + nivel;
+}
+
+// Exemplo de uso da função
+let vitorias = 79;
+let derrotas = 23;
+let resultado = calcularNivel(vitorias, derrotas);
+
+console.log(resultado);
